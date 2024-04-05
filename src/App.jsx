cf70df8964639a493f28c064ddfad64d7bbd4060
touchavin <touchavin@hotmail.com>
2024-04-05 14:09:49 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Routes, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
       <Routes>
-        
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
   )
