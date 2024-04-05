2999d7be0e63b61fa29ef7b216bad396461b0ae0
touchavin <touchavin@hotmail.com>
2024-04-05 13:39:11 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Routes, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        
+      </Routes>
     </div>
   )
 }
