e92ecdfeab5ab5aca481e7fb5b2a45ae04ae4549
touchavin <touchavin@hotmail.com>
2024-04-05 13:35:35 +0700

    up

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
             <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
             <img src={assets.basket_icon} alt="" />
+            <div className='dot'></div>
         </div>
         <div className="dot"></div>
          <button>sign in</button>
