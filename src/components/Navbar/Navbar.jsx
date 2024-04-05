0297995884f32022be20a9b493c716bcb85c4eeb
touchavin <touchavin@hotmail.com>
2024-04-05 11:02:14 +0700

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,17 @@ import {assets} from '../../assets/assets'
 
 const Navbar = () => {
 
-      const [menu, setMenu] =useState("home");
+      const [menu, setMenu] =useState("menu");
 
 
   return (
     <div className='navbar'>
         <img src={assets.logo} alt=''/>
         <ul className="navbar-menu">
-            <li className={menu==="home"?"active":""}>home</li>
-            <li className={menu==="menu"?"active":""}>menu</li>
-            <li className={menu==="mobile-app"?"active":""}>mobile-app</li>
-            <li className={menu==="contact us"?"active":""}>contact us</li>
+            <li onClick={()=> setMenu("home")} className={menu==="home"?"active":""}>home</li>
+            <li onClick={()=> setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+            <li onClick={()=> setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+            <li onClick={()=> setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</li>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
