aefa7f72ef5ea640546e5cbae8cd670800ec241a
touchavin <touchavin@hotmail.com>
2024-04-05 14:13:11 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import { Routes, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 
 const App = () => {
